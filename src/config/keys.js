baf010ad4472b706305d6654a7f436c4d2e8573f
Ramon Julia <reimon1972@gmail.com>
2019-09-06 18:54:38 +0800

    add env file

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,7 @@
+const api = process.env.REACT_APP_API
+
 export const config = {
-  apiUri: 'https://newsapi.org/v2/everything?domains=washingtonpost.com, nytimes.com&from=2019-08-06&sortBy=publishedAt&apiKey=a60fa12292c149068eadc6d49e481337',
+  apiUri: api,
   apiKey: 'a60fa12292c149068eadc6d49e481337'
 }
 
