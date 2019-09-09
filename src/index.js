474721ca46a72a7c1ab3473af55723beb0cbd89c
Ramon Julia <reimon1972@gmail.com>
2019-09-09 10:41:39 +0800

    delete default service worket

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import rootReducer from './state/reducers/index';
 
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers =
+  typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const middleware = [thunk];
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware)
+);
+const store = createStore(rootReducer, enhancer);
 
 //store.dispatch(getAllPosts(15));
 
@@ -19,4 +29,3 @@ ReactDOM.render(
     <App />
   </Provider>, document.getElementById('root'));
 
-serviceWorker.unregister();
